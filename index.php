3b334d831a311335d0e34f959095dc362af31f4b
GabrielGamboa-Codex <foxygamboafnaf2003@gmail.com>
2025-01-10 13:51:07 -0400

    Pestañas con Bootrap y ajax

--- index.php
+++ index.php
@@ -1,47 +1,40 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bootstrap Tabs Example</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Pestañas</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="container mt-3">
-        <div class="border rounded bg-white p-3">
-            <h2>Ejemplo de Pestañas</h2>
-            <br>
-            <br>
-            <button id="loadTabs" class="btn btn-primary">Mostrar Pestañas</button>
-            <button id="hideTabs" class="btn btn-secondary" style="display: none;">Ocultar Pestañas</button>
-            <br>
-            <br>
-            <div id="tabsContainer" style="display: none;">
-                <ul class="nav nav-tabs border rounded" id="myTab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true" data-url="./views/home.php">Home</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="menu1-tab" data-bs-toggle="tab" data-bs-target="#menu1" type="button" role="tab" aria-controls="menu1" aria-selected="false" data-url="./views/menu1.php">Spawn</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="menu2-tab" data-bs-toggle="tab" data-bs-target="#menu2" type="button" role="tab" aria-controls="menu2" aria-selected="false" data-url="./views/menu2.php">Batman</button>
-                    </li>
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                    </div>
-                    <div class="tab-pane fade" id="menu1" role="tabpanel" aria-labelledby="menu1-tab">
-                    </div>
-                    <div class="tab-pane fade" id="menu2" role="tabpanel" aria-labelledby="menu2-tab">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <br>
+    <br>
+    <h1 class="col text-center">Pestañas</h1>
+<br>
+<div class="container mt-2 p-2">
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
+        </li>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="./js/jquery-3.7.1.min.js"></script>
-    <script src="./js/ajax.js"></script>
+        <li class="nav-item" role="presentation">
+        <button class="nav-link" id="batman-tab" data-bs-toggle="tab" data-bs-target="#batman" type="button" role="tab" aria-controls="batman" aria-selected="false">Batman</button>
+        </li>
+
+        <li class="nav-item" role="presentation">
+        <button class="nav-link" id="spawn-tab" data-bs-toggle="tab" data-bs-target="#spawn" type="button" role="tab" aria-controls="spawn" aria-selected="false">Spawn</button>
+        </li>
+    </ul>
+</div>
+
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"></div>
+  <div class="tab-pane fade" id="batman" role="tabpanel" aria-labelledby="batman-tab"></div>
+  <div class="tab-pane fade" id="spawn" role="tabpanel" aria-labelledby="spawn-tab"></div>
+</div>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/ajax.js"></script>
 </body>
 </html>
--- views/menu1.php
+++
@@ -1,9 +0,0 @@
-<body>
-    <div class="container mt-3 p-3">
-        <p>Spawn es un Engendro Infernal enviado a la Tierra para entregar almas malvadas al Infierno. Sin embargo, Spawn se convirtió en el pseudo-protector de la Ciudad de New York, luchando contra fuerzas del Cielo y el Infierno por igual. El legado de los Engendros Infernales se extiende a lo largo de la historia aprovechando el enigmático Necroplasma y criaturas siniestras (como gusanos, murciélagos y lobos), para obtener sus poderes. Medieval Spawn fue uno de los Engendros Infernales anteriores y fue asesinado por Angela, una guerrera seráfica. Albert Simmons fue el Engendro Infernal del siglo XX surgido durante la década 1990. Se sacrificó para liberarse de la tortura eterna, pero reanimó a James Downing como resultado de su acción. Jim cargó el manto de Engendro Infernal durante varios años hasta el regreso de Al en 2015.</p>
-        <div class="row">
-            <div class="col text-center">
-                <img src="views/img/spawn_1.jpg" class="img-fluid mx-auto d-block">
-            </div>
-        </div>
-</body>
